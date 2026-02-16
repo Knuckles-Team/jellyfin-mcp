f773888c2096896aebd91c51d6eae93f49557202
KnucklesSG1 <knucklessg1@gmail.com>
2026-02-15 18:37:37 -0600

    Bump version: 0.2.8 → 0.2.9

--- jellyfin_mcp/jellyfin_agent.py
+++ jellyfin_mcp/jellyfin_agent.py
@@ -39,7 +39,7 @@ from jellyfin_mcp.utils import (
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-__version__ = "0.2.8"
+__version__ = "0.2.9"
 
 AGENT_NAME = "JellyfinAgent"
 AGENT_DESCRIPTION = (
